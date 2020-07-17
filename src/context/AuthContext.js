7b1310977ed4b3bc492c43ec2ab047e62e625d72
RGaldamez <ricardo.j.galdamez@gmail.com>
2020-07-17 15:38:04 -0600

    Enabling app to communicate with backend via API, backend returns jwt succesfully

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,5 @@
 import createDataContext from './createDataContext';
+import trackerApi from '../api/tracker';
 
 const authReducer = (state, action) => {
   switch (action.type) {
@@ -7,8 +8,36 @@ const authReducer = (state, action) => {
   }
 };
 
+const signup = (dispatch) => {
+  return async ({email, password}) => {
+    //axios request to backend
+    //user signs up, change state to authorize use
+    //sign up fails, show error
+    try {
+      const response = await trackerApi.post('/signup', {email, password});
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+};
+
+const signin = (dispatch) => {
+  return ({email, password}) => {
+    //axios request to login
+    //change state to authorize use
+    //sign in fails, show error
+  };
+};
+
+const signout = (dispatch) => {
+  return () => {
+    //axios request to logout
+  };
+};
+
 export const {Provider, Context} = createDataContext(
   authReducer,
-  {},
+  {signup, signin, signout},
   {isSignedIn: false},
 );
